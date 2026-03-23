ca8b95ce593e760e277610a2cbc7eefb27a9dbe2
omkrfig <omkrfig@gmail.com>
2026-03-22 21:01:58 -0400

    Adds for contact, mixins comments to not to break anything

--- src/functions/Home/navbar.js
+++ src/functions/Home/navbar.js
@@ -1,6 +1,9 @@
 const links = document.querySelectorAll('.nav__list a');
 
 links.forEach(link => {
+    if (link.href === window.location.href) {
+        link.classList.add('active');
+    }
     link.addEventListener('click', () => {
         links.forEach(l => l.classList.remove('active'));
         link.classList.add('active');
